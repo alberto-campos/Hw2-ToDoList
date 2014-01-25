27e1510508651ebbc42626766bdf7d983d948482
Alberto Campos <alberto_campos@symantec.com>
2014-01-25 14:42:43 -0800

    Initial Commit

---
+++ Hw2-ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hw2-ToDoList
+//
+//  Created by Alberto Campos on 1/25/14.
+//  Copyright (c) 2014 Alberto Campos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
